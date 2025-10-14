03dd67126b3c4c1876609095323f90a9cdccdede
Vĩnh Hùng <tatvinhhung2004@gmail.com>
2025-10-14 08:46:32 +0700

    add spring backend to develop branch

---
+++ test_order_service/src/main/java/com/example/test_order_service/TestOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOrderServiceApplication.class, args);
+    }
+
+}
