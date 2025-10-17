35f1ade5e4a66e4e0a105d483659a0437498d4bb
Vĩnh Hùng <tatvinhhung2004@gmail.com>
2025-10-17 10:52:57 +0700

    [FEAT] Add validation

--- backend/src/main/java/com/example/test_order_service/dto/request/TestOrderUpdateRequest.java
+++ backend/src/main/java/com/example/test_order_service/dto/request/TestOrderUpdateRequest.java
@@ -1,14 +1,19 @@
 package com.example.test_order_service.dto.request;
 
 import com.example.test_order_service.entity.enumForEntity.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.regex.Pattern;
 
 @Data
 public class TestOrderUpdateRequest {
     private String patientName;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dateOfBirth;
+
     private Integer age;
     private Gender gender;
     private String phone;
