1ae6a3c42d2f74f2c13cb4220c1fa0c1e49a8931
TranPhuocAn <antpse180115@fpt.edu.vn>
2025-10-31 14:16:18 +0700

    [FEAT] Wait for Instrument Service if no response.

--- backend/src/main/java/com/example/test_order_service/serviceImpl/TestOrderServiceImpl.java
+++ backend/src/main/java/com/example/test_order_service/serviceImpl/TestOrderServiceImpl.java
@@ -33,6 +33,7 @@ public class TestOrderServiceImpl implements TestOrderService {
     private final TestOrderMapper testOrderMapper;
     private final TestResultMapper testResultMapper;
     private final CommentMapper commentMapper;
+    private final InstrumentSyncService instrumentSyncService;
     
     // Optional: Event publisher sẽ chỉ inject nếu có sẵn
     @Autowired(required = false)
@@ -56,6 +57,11 @@ public class TestOrderServiceImpl implements TestOrderService {
         }
 
         TestOrderResponse response = testOrderMapper.toTestOrderResponse(savedOrder);
+
+        try {
+            instrumentSyncService.requestOrWait(savedOrder.getTestOrderId());
+        } catch (Exception ex) {
+        }
 
         return RestResponse.<TestOrderResponse>builder()
                 .statusCode(200)
